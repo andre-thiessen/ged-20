cbd630121b0a2cbfde24ddb6feefa53457256521
andre-thiessen <almthiessen@gmail.com>
2025-06-12 22:25:36 -0300

    update create_label function with drain_rate

--- utils/data_prep.py
+++ utils/data_prep.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def create_label(df: pd.DataFrame) -> pd.DataFrame:
+    EPS = 0.1   # evita divisão por zero para devices sem son_hrs registrado
+    df["drain_rate"] = df["dchrg_diff"] / (df["son_hrs"] + EPS)   # % por hora  
+    thr_rate = df["drain_rate"].quantile(0.90)
+    
     def make_label(row):
-        return int(
-            (row["dchrg_diff"] > 25)
-            and ((row["full_cap_min"] < 2800) or (row["btlow_total"] > 10))
-        )
+        drain_flag = row["drain_rate"] > thr_rate
+        health_flag = (row["full_cap_min"] < 2800)
+        return int(drain_flag and health_flag)
+    
     df["label_ineficiente"] = df.apply(make_label, axis=1)
     return df
